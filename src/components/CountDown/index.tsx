e11c23d79a61824103c17e792d3eae90e85a4026
Thiago Aragão <tbaragao@gmail.com>
2025-05-03 22:57:56 -0300

    feat: starts the mainForm implementation

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -2,7 +2,7 @@ import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import style from './styles.module.css';
 
 export function CountDown() {
-  const { state, setState } = useTaskContext();
+  const { state } = useTaskContext();
   return (
     <>
       <div className={style.container}>{state.formattedSecondsRemaining}</div>
